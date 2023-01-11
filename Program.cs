48977551544d12648448eb51e996e19d34f60501
KazarinMichael <kazarin1984m@gmail.com>
2023-01-12 00:04:56 +0500

    создали программу

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int[] arrey = { 1, 12, 31, 4, 15, 16, 17, 18};
+
+int n = arrey.Lenght;
+int find = 4;
+
+int index = 0;
+
+while (index < n);
+{
+
+    if(arrey[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = + 1;
+    index++;
+}
+
+
+
+
+
+
+
+
